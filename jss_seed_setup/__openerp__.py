3388c3be32ef4b833850ddc2182a30c4d1b4b91d
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-04-05 12:02:21 +0530

    Banka | #810 | 1) Adding jss_seed_setup module.                2) Adding seed data for product_category.

---
+++ jss_seed_setup/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Bahmni Seed Data setup",
+    "version": "1.0",
+    "depends": ["base","product"],
+    "author": "ThoughtWorks Technologies Pvt. Ltd.",
+    "category": "Setup",
+	"summary": "Initial data setup",
+    "description": """
+    """,
+    'data': ['data/product_category_setup.xml'],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
