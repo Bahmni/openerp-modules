7aea4e3cbde2f5238019c8e8421d582777baf810
pchandra <pchandra@thoughtworks.com>
2013-04-05 11:53:40 +0530

    praveen| adding a dummy module with dependancies to all required modules to make installation simple

---
+++ jss_module_install/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Bahmny Module Install",
+    "version": "6.0",
+    "depends": ["base","product","account","account_voucher","purchase","account_accountant","sale","purchase","product_expiry","jss_pharmacy_product","jss_stock_batch_sale_price","jss_sale_discount","jss_customer_payment"],
+    "author": "ThoughtWorks Technologies Pvt. Ltd.",
+    "category": "Setup",
+	"summary": "Install Module",
+    "description": """
+    """,
+    'data': [],
+    'demo': [],
+    'auto_install': True,
+    'application': True,
+    'installable': True,
+#    'certificate': 'certificate',
+}
