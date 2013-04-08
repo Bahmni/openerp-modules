0a9a63cea39d6dbe9568b50ef99e6be6e3fe9eff
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-04-08 18:14:29 +0530

    Banka | Stock internal move module creation

---
+++ bahmni_internal_stock_move/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Bahmni Internal Stock Move",
+    "version": "1.0",
+    "depends": ["base","stock"],
+    "author": "ThoughtWorks Technologies Pvt. Ltd.",
+    "category": "Stock",
+    "summary": "Stock internal move",
+    "description": """
+    """,
+    'data': ['stock_internal_move_view.xml'],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
