b687ecd658aaaacadb03e53c211b570664e8a884
sushmitharaos <sushmitharaos@gmail.com>
2013-04-19 14:58:58 +0530

    Banka, Sush | #00 | preselect checkbox to select existing batch numbers for internal moves.

--- bahmni_internal_stock_move/stock_move.py
+++ bahmni_internal_stock_move/stock_move.py
@@ -7,7 +7,6 @@ import decimal_precision as dp
 from openerp import netsvc
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
 
-
 class stock_move(osv.osv):
     _name = "stock.move"
     _inherit = "stock.move"
@@ -47,3 +46,25 @@ class stock_move(osv.osv):
             }
 
         return {'warning': warning}
+
+class split_in_production_lot(osv.osv_memory):
+    _name = "stock.move.split"
+    _inherit = "stock.move.split"
+
+    def default_get(self, cr, uid, fields, context=None):
+        if context is None:
+            context = {}
+        res = super(split_in_production_lot, self).default_get(cr, uid, fields, context=context)
+        if context.get('active_id'):
+            move = self.pool.get('stock.move').browse(cr, uid, context['active_id'], context=context)
+            if 'product_id' in fields:
+                res.update({'product_id': move.product_id.id})
+            if 'product_uom' in fields:
+                res.update({'product_uom': move.product_uom.id})
+            if 'qty' in fields:
+                res.update({'qty': move.product_qty})
+            if 'use_exist' in fields:
+                res.update({'use_exist': (move.picking_id and (move.picking_id.type=='out' or move.picking_id.type=='internal') and True) or False})
+            if 'location_id' in fields:
+                res.update({'location_id': move.location_id.id})
+        return res
