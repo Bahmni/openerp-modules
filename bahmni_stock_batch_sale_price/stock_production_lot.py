4eafbf232e1e0be4aad14c6667d47ea6157412ab
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-04-23 16:31:16 +0530

    Banka | Making expiry dates field to be date instead of datetime

--- bahmni_stock_batch_sale_price/stock_production_lot.py
+++ bahmni_stock_batch_sale_price/stock_production_lot.py
@@ -14,6 +14,14 @@ class jss_production_lot(osv.osv):
         'sale_price':fields.float('Sale Price',digits=(4,2)),
         'mrp':fields.float('MRP',digits=(4,2)),
         'cost_price':fields.float('Cost Price',digits=(4,2)),
+        'life_date': fields.date('End of Life Date',
+            help='This is the date on which the goods with this Serial Number may become dangerous and must not be consumed.'),
+        'use_date': fields.date('Best before Date',
+            help='This is the date on which the goods with this Serial Number start deteriorating, without being dangerous yet.'),
+        'removal_date': fields.date('Removal Date',
+            help='This is the date on which the goods with this Serial Number should be removed from the stock.'),
+        'alert_date': fields.date('Alert Date',
+            help="This is the date on which an alert should be notified about the goods with this Serial Number."),
 
         }
 
