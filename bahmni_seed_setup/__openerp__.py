950f0b41681b0f80a788c6f3813949de81f06531
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-05-09 16:16:33 +0530

    Banka | #860 | 1) Deleting default shop & location.                2) Changing details of default company.                3) Changing Date format to dd/mm/yyyy

--- bahmni_seed_setup/__openerp__.py
+++ bahmni_seed_setup/__openerp__.py
@@ -7,7 +7,7 @@
 	"summary": "Initial data setup",
     "description": """
     """,
-    'data': ['data/product_category_setup.xml', 'data/suppliers_seed_setup.xml', 'data/warehouse_seed_setup.xml', 'data/filters.xml'],
+    'data': ['data/settings.xml', 'data/product_category_setup.xml', 'data/suppliers_seed_setup.xml', 'data/warehouse_seed_setup.xml', 'data/filters.xml'],
     'demo': [],
     'auto_install': False,
     'application': True,
