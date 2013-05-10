a4a1f688295ec292a0cb7e713d8f178a93bd6199
Vinkesh Banka <vinkesh.banka@gmail.com>
2013-05-10 16:01:21 +0530

    Banka | #884 | Slightly changing locations and warehouse seed data.

--- bahmni_seed_setup/__openerp__.py
+++ bahmni_seed_setup/__openerp__.py
@@ -1,7 +1,7 @@
 {
     "name": "Bahmni Seed Data setup",
     "version": "1.0",
-    "depends": ["base","product", "stock", "sale"],
+    "depends": ["base","product", "stock", "sale", "sale_stock"],
     "author": "ThoughtWorks Technologies Pvt. Ltd.",
     "category": "Setup",
 	"summary": "Initial data setup",
