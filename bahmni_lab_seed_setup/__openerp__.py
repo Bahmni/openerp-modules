dccfcd95dfe7c93770dc123bf136baf1b359c0f4
pchandra <pchandra@thoughtworks.com>
2013-08-29 16:25:37 +0530

    praveen: adding lab inventory seed data

---
+++ bahmni_lab_seed_setup/__openerp__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Bahmni Lab Seed Data setup",
+    "version": "1.0",
+    "depends": ["base","product", "stock"],
+    "author": "ThoughtWorks Technologies Pvt. Ltd.",
+    "category": "Setup",
+	"summary": "Initial data setup",
+    "description": """
+    """,
+    'data': [
+        'data/settings.xml',
+        'data/account_heads.xml',
+        'data/product_category_setup.xml',
+        'data/suppliers_seed_setup.xml',
+        'data/warehouse_seed_setup.xml',
+        'data/unit_of_measures.xml',
+        'data/payment_method.xml',
+        'data/products.xml',
+    ],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
