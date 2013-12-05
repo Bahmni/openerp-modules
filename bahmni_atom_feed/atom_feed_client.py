ce812b8078a8206853e244bebc2438c79c7cb4ea
pchandra <pchandra@thoughtworks.com>
2013-12-05 10:13:47 +0530

    praveen|not throw exception if feed_uri is empty or has '$param' values (this might happen for failed event processing)

--- bahmni_atom_feed/atom_feed_client.py
+++ bahmni_atom_feed/atom_feed_client.py
@@ -116,8 +116,8 @@ class atom_event_worker(osv.osv):
         feed_uri_for_last_read_entry = vals.get('feed_uri_for_last_read_entry')
         feed_uri = vals.get('feed_uri')
 
-        if "$param" in feed_uri_for_last_read_entry or "$param" in feed_uri :
-            raise osv.except_osv(('Error!'),("Junk values in feed urls"))
+        if "$param" in feed_uri_for_last_read_entry or "$param" in feed_uri or feed_uri_for_last_read_entry == None:
+            return
 
         marker_ids = self.pool.get('atom.feed.marker').search(cr, uid, [('feed_uri', '=', feed_uri)], limit=1)
 
