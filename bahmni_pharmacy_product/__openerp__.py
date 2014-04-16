00f3c17d52b0fde4d62e95c61911e761df05f939
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-04-16 15:51:36 +0530

    Indraneel, Banka | #2064 | Disallowing stock user from editing a product

--- bahmni_pharmacy_product/__openerp__.py
+++ bahmni_pharmacy_product/__openerp__.py
@@ -1,7 +1,7 @@
 {
     "name": "Bhamni Pharmacy Product",
     "version": "6.0",
-    "depends": ["base","product", "stock","bahmni_atom_feed"],
+    "depends": ["base","product", "stock","bahmni_atom_feed", "purchase"],
     "author": "ThoughtWorks tecnologies Pvt. Ltd.",
     "category": "Product",
 	"summary": "Pharmacy Product",
