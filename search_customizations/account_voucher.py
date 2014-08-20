6749e942e07d566b6a1c01a5a6fd23955040a84f
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-08-20 16:57:59 +0530

    Banka | #565 | Fixing a bug in auto populating 'Paid Amount' field in customer payment in case of refund.

--- search_customizations/account_voucher.py
+++ search_customizations/account_voucher.py
@@ -1,16 +1,18 @@
 from openerp.osv import fields, osv
 import openerp.addons.decimal_precision as dp
 from openerp.tools.translate import _
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class account_voucher(osv.osv):
     _name = 'account.voucher'
     _inherit = "account.voucher"
 
-
     def onchange_partner_id(self, cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context=None):
         res = super(account_voucher, self).onchange_partner_id(cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context=context)
-        if(res.get('value', False) and res['value'].get('balance_amount', False)):
-            res['value']['amount'] = res['value']['balance_amount']
+        if(res.get('value', False) and partner_id):
+            partner = self.pool.get('res.partner').browse(cr, uid, partner_id, context=context)
+            res['value']['amount'] = partner.credit if partner is not None else 0.0
         return res
 
