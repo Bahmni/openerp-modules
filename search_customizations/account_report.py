d29090bbfc168a63778ebc99e91ec9a76c754f18
indraneelr <indraneel.rr@gmail.com>
2015-01-23 11:26:49 +0530

    indraneel | fixing the search account report query to handle grouping by invoice_id

--- search_customizations/account_report.py
+++ search_customizations/account_report.py
@@ -20,62 +20,62 @@ class account_report(osv.osv):
         drop_view_if_exists(cr, 'account_report')
         cr.execute("""
             create or replace view account_report as (
-                (select id,date,account_id, sum(actual_amount) as actual_amount,sum(amount_received) as amount_received from (
-                  SELECT
-                    pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,
-                    ai.date_invoice AS date,
-                    ail.account_id,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN 0
-                    ELSE sum(ail.price_subtotal * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
-                    END AS actual_amount,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN sum(
-                        (-ail.price_subtotal) * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
-                    ELSE sum(ail.price_subtotal * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
-                    END AS amount_received
-                  FROM account_invoice ai, account_invoice_line ail
-                  WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) <> 0 AND state = 'paid'
-                  GROUP BY ail.account_id, ai.date_invoice, ai.type
-                  UNION
-                  SELECT
-                    pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,
-                    ai.date_invoice AS date,
-                    ail.account_id,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN 0
-                    ELSE max(ai.amount_total)
-                    END AS actual_amount,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN max(ai.amount_total) * -1
-                    ELSE max(ai.amount_total)
-                    END AS amount_received
-                  FROM account_invoice ai, account_invoice_line ail
-                  WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) = 0 AND state = 'paid'
-                        and ail.account_id not in
-                        (select id from account_account where name in ('FINE','Discount','Overcharge'))
-                  GROUP BY ail.account_id, ai.date_invoice, ai.type
-                  UNION
-                  SELECT
-                    pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,
-                    ai.date_invoice AS date,
-                    ail.account_id,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN 0
-                    ELSE sum(ai.amount_total)
-                    END AS actual_amount,
-                    CASE
-                    WHEN ai.type = 'out_refund' THEN sum(-ai.amount_total)
-                    ELSE sum(ai.amount_total)
-                    END AS amount_received
-                  FROM account_invoice ai, account_invoice_line ail
-                  WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) = 0 AND state = 'paid'
-                        and ail.account_id in
-                        (select id from account_account where name in ('FINE','Discount','Overcharge'))
-                  GROUP BY ail.account_id, ai.date_invoice, ai.type
-                ) as r group by id,date,account_id)
+              (select id,date,account_id, sum(actual_amount) as actual_amount,sum(amount_received) as amount_received
+               from (
+               SELECT
+                 pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,ai.id as invoice_id,
+                 ai.date_invoice AS date,
+                 ail.account_id,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN 0
+                 ELSE sum(ail.price_subtotal * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
+                 END AS actual_amount,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN sum(
+                     (-ail.price_subtotal) * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
+                 ELSE sum(ail.price_subtotal * (ai.amount_total / (ai.amount_tax + ai.amount_untaxed)))
+                 END AS amount_received
+               FROM account_invoice ai, account_invoice_line ail
+               WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) <> 0 AND state = 'paid'
+               GROUP BY ail.account_id, ai.date_invoice, ai.type, ai.id
+               UNION
+               SELECT
+                 pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,ai.id as invoice_id,
+                 ai.date_invoice AS date,
+                 ail.account_id,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN 0
+                 ELSE max(ai.amount_total)
+                 END AS actual_amount,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN max(ai.amount_total) * -1
+                 ELSE max(ai.amount_total)
+                 END AS amount_received
+               FROM account_invoice ai, account_invoice_line ail
+               WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) = 0 AND state = 'paid'
+                     and ail.account_id not in
+                         (select id from account_account where name in ('FINE','Discount','Overcharge'))
+               GROUP BY ail.account_id, ai.date_invoice, ai.type,ai.id
+               UNION
+               SELECT
+                 pg_catalog.concat(ail.account_id, '_', ai.date_invoice, '_', ai.type) AS id,ai.id as invoice_id,
+                 ai.date_invoice AS date,
+                 ail.account_id,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN 0
+                 ELSE sum(ai.amount_total)
+                 END AS actual_amount,
+                 CASE
+                 WHEN ai.type = 'out_refund' THEN sum(-ai.amount_total)
+                 ELSE sum(ai.amount_total)
+                 END AS amount_received
+               FROM account_invoice ai, account_invoice_line ail
+               WHERE ail.invoice_id = ai.id AND (ai.amount_tax + ai.amount_untaxed) = 0 AND state = 'paid'
+                     and ail.account_id in
+                         (select id from account_account where name in ('FINE','Discount','Overcharge'))
+               GROUP BY ail.account_id, ai.date_invoice, ai.type,ai.id
+             ) as r group by id,date,account_id)
             )""")
-
     def unlink(self, cr, uid, ids, context=None):
         raise osv.except_osv(_('Error!'), _('You cannot delete any record!'))
 
