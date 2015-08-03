b9cf1d93686d7d579b7417806f601fd936efd0f2
bharatak <bharatak@thoughtworks.com>
2015-08-04 05:04:20 +0530

    Bharat,Banka|#2412 Added expiry date of the drug in sales order and the bill

---
+++ bahmni_stock_batch_sale_price/account_invoice_line.py
@@ -0,0 +1,16 @@
+import logging
+
+from openerp.osv import fields, osv
+
+_logger = logging.getLogger(__name__)
+
+class account_invoice_line(osv.osv):
+    _name = "account.invoice.line"
+    _inherit = "account.invoice.line"
+    _columns = {
+        # 'batch_id': fields.many2one('stock.production.lot', 'Batch No'),
+        'batch_name': fields.char('Batch No'),
+        'expiry_date': fields.date('Expiry Date'),
+    }
+
+account_invoice_line()
