b13f94e5500520ed32cea8af1a559502a7de90a9
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-08-27 17:59:05 +0530

    Gourav, Shireesha, Banka | #2772 | Added a new module to allow moving old stock from any location to other location.

---
+++ bahmni_stock_fix/__init__.py
@@ -0,0 +1,1 @@
+import old_prodlot_move
---
+++ bahmni_stock_fix/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Bahmni stock fix",
+    "version": "1.0",
+    "depends": ["base", "bahmni_internal_stock_move"],
+    "author": "ThoughtWorks Technologies Pvt. Ltd.",
+    "category": "",
+    "summary": "Code to create internal moves for old stock batches.",
+    "description": """
+    """,
+    'data': ['old_prodlot_move_view.xml'],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
