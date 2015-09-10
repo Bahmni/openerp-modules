2492a792c1b39ce6f63dcc9c52b3515b9780143d
abishek91 <abishek91@gmail.com>
2015-09-10 10:16:05 +0530

    Abishek/Hanisha | 2816 | pick-up-correct-batches-for-drug-order-entry-based-on-user-s-login-location

--- bahmni_atom_feed/order_type_shop_map.py
+++ bahmni_atom_feed/order_type_shop_map.py
@@ -6,6 +6,7 @@ class order_type_shop_map(osv.osv):
     _columns = {
         'order_type': fields.char('Order Type', required=True, size=64),
         'shop_id': fields.many2one('sale.shop', 'Shop', required=True),
+        'location_name' : fields.char('Location Name')
     }
 
 order_type_shop_map()
