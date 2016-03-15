c8f06ddafab46fe7d73085d307877c5e96d171e0
Pankaj <ladharpankaj@gmail.com>
2016-03-15 14:43:37 +0530

    Pankaj, Achinta | #1134 |
    Added the functionality to navigate to dispensed/un-dispensed quotations directly from the app.
    If the quotations are not present it takes by default to the quotation list page.

--- bahmni_web_extensions/quotation_controller.py
+++ bahmni_web_extensions/quotation_controller.py
@@ -18,7 +18,7 @@ class QuotationController(openerp.addons.web.http.Controller):
     _cp_path = '/quotations'
 
     @openerpweb.httprequest
-    def latest(self, req, patient_ref):
+    def latest(self, req, patient_ref,dispensed):
         uid = req.session._uid or 1
         dbname = req.session._db or "openerp"
         context = req.session.context or {}
@@ -26,10 +26,33 @@ class QuotationController(openerp.addons.web.http.Controller):
         with registry.cursor() as cr:
             pool = pooler.get_pool(dbname)
             patient_id = pool.get('res.partner').search(cr, uid, [('ref', '=', patient_ref)])
-            quotation_ids = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC")])
+
+            sale_order_ids = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC")])
+
+            sale_order_id_dispensed = []
+            sale_order_id_nondispensed = []
+
+            for sale_order_id in sale_order_ids:
+                sale_order_line_ids = pool.get('sale.order.line').search(cr, uid, [('order_id', '=', sale_order_id)], context=context)
+                sale_order_lines = pool.get('sale.order.line').browse(cr, uid, sale_order_line_ids[0], context=context)
+                if(sale_order_lines.dispensed_status == 'true'):
+                    sale_order_id_dispensed.append(sale_order_id)
+                else:
+                    sale_order_id_nondispensed.append(sale_order_id)
+
             redirect_link = "/#view_type=list&model=sale.order&menu_id=296&action=372"
-            if(len(quotation_ids) > 0):
-                redirect_link = "/#id={0}&view_type=form&model=sale.order&menu_id=296&action=372".format(quotation_ids[0])
+
+            if(dispensed == 'true') :
+                if(len(sale_order_id_dispensed) > 0):
+                    quotation_id = sale_order_id_dispensed[0]
+                    redirect_link = "/#id={0}&view_type=form&model=sale.order&menu_id=296&action=372".format(quotation_id)
+            else:
+                if(len(sale_order_id_nondispensed) > 0):
+                    quotation_id = sale_order_id_nondispensed[0]
+                    redirect_link = "/#id={0}&view_type=form&model=sale.order&menu_id=296&action=372".format(quotation_id)
+
+            # if(len(sale_order_ids) > 0):
+
 
             return """
             <html>
