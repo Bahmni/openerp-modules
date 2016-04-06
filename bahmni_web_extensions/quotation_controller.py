a245455b6b6965f09bd985c5fc5d8b0df3a6e943
Pankaj <ladharpankaj@gmail.com>
2016-04-06 15:30:08 +0530

    Pankaj | #1435 | Added new parameter called 'location_ref' to support open dispensed quotation irrespective of login location.

--- bahmni_web_extensions/quotation_controller.py
+++ bahmni_web_extensions/quotation_controller.py
@@ -18,40 +18,61 @@ class QuotationController(openerp.addons.web.http.Controller):
     _cp_path = '/quotations'
 
     @openerpweb.httprequest
-    def latest(self, req, patient_ref,dispensed):
+    def latest(self, req, patient_ref,dispensed,location_ref):
         uid = req.session._uid or 1
         dbname = req.session._db or "openerp"
         context = req.session.context or {}
         registry = RegistryManager.get(dbname)
         with registry.cursor() as cr:
-            pool = pooler.get_pool(dbname)
+            pool = pooler.get_pool(dbname);
             patient_id = pool.get('res.partner').search(cr, uid, [('ref', '=', patient_ref)])
 
-            sale_order_ids = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC")])
+            shop_id = None
+            local_shop_id = None
 
-            sale_order_id_dispensed = []
-            sale_order_id_nondispensed = []
+            shop_mapping = pool.get('order.type.shop.map').search(cr, uid, [('location_name', '=', location_ref)])
+            if shop_mapping :
+                shop_id = pool.get('order.type.shop.map').browse(cr, uid, shop_mapping[0], context=context).shop_id
+                local_shop_id = pool.get('order.type.shop.map').browse(cr, uid, shop_mapping[0], context=context).local_shop_id
 
-            for sale_order_id in sale_order_ids:
-                sale_order_line_ids = pool.get('sale.order.line').search(cr, uid, [('order_id', '=', sale_order_id)], context=context)
-                sale_order_lines = pool.get('sale.order.line').browse(cr, uid, sale_order_line_ids[0], context=context)
-                if(sale_order_lines.dispensed_status == 'true'):
-                    sale_order_id_dispensed.append(sale_order_id)
-                else:
-                    sale_order_id_nondispensed.append(sale_order_id)
+            sale_order_id_nondispensed =[]
 
-            redirect_link = "/#view_type=list&model=sale.order&menu_id=296&action=372"
+            if shop_id != None:
+                sale_order_id_nondispensed = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC"),('shop_id','=',shop_id.id)])
+            else:
+                sale_order_ids = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC")])
+                for sale_order_id in sale_order_ids:
+                    sale_order_line_ids = pool.get('sale.order.line').search(cr, uid, [('order_id', '=', sale_order_id)], context=context)
+                    sale_order_lines = pool.get('sale.order.line').browse(cr, uid, sale_order_line_ids[0], context=context)
+                    if(sale_order_lines.dispensed_status == 'false'):
+                        sale_order_id_nondispensed.append(sale_order_id)
+
+
+            if local_shop_id != None:
+                sale_order_id_dispensed = pool.get('sale.order').search(cr, uid, [('partner_id', 'in', patient_id), ('state', '=', 'draft'), ('origin', '=', "ATOMFEED SYNC"),('shop_id','=',local_shop_id.id)])
+            else:
+                sale_order_id_dispensed =[];
+
+
+
+
 
             if(dispensed == 'true') :
                 if(len(sale_order_id_dispensed) > 0):
                     quotation_id = sale_order_id_dispensed[0]
                     redirect_link = "/#id={0}&view_type=form&model=sale.order&menu_id=296&action=372".format(quotation_id)
-            else:
+                else:
+                    redirect_link = "/#view_type=list&model=sale.order&menu_id=296&action=372"
+
+            elif dispensed == 'false':
                 if(len(sale_order_id_nondispensed) > 0):
                     quotation_id = sale_order_id_nondispensed[0]
                     redirect_link = "/#id={0}&view_type=form&model=sale.order&menu_id=296&action=372".format(quotation_id)
+                else:
+                    redirect_link = "/#view_type=list&model=sale.order&menu_id=296&action=372"
 
-            # if(len(sale_order_ids) > 0):
+            else:
+                redirect_link = "/#view_type=list&model=sale.order&menu_id=296&action=372"
 
 
             return """
